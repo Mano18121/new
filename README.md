a27f1e3a848bf1f77963c7e3f98ae91b899a885d
Mano18121 <105213131+Mano18121@users.noreply.github.com>
2022-05-09 17:39:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# new
+# Crop-Prediction
---
+++ app.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 09:04:15 2022
+
+@author: karthikeyan
+"""
+
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model=pickle.load(open('prediction.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('frontpage.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    
+    
+    -------
+    None.
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction=model.predict(final_features)
+   
+    
+    output = prediction[0]
+    text= "The Recommended Crop is "
+    return render_template("result.html",output=text+output)
+    #return render_template('index.html',prediction_text='Employee Salary should be $ {}'.format(output))
+
+if __name__=="__main__":
+    app.run(debug=True)
